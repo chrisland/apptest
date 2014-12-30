b7ccceed8824e3f9e707553f2d74473ac60db917
christian marienfeld <post@chrisland.de>
2014-12-30 17:36:30 +0100

    test version

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,17 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        alert('jo');
+        
+        var element = document.getElementById('deviceProperties');
+        element.innerHTML = 'Device Model: '    + device.model    + '<br />' +
+                            'Device Cordova: '  + device.cordova  + '<br />' +
+                            'Device Platform: ' + device.platform + '<br />' +
+                            'Device UUID: '     + device.uuid     + '<br />' +
+                            'Device Version: '  + device.version  + '<br />';
+
+
+
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
